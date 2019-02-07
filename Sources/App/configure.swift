06a2039873a8f06bb8ec8279ab16a3a90d643727
macbookair <464477066@qq.com>
2019-02-07 21:12:04 +0800

    push current files

---
+++ Sources/App/configure.swift
@@ -0,0 +1,58 @@
+import Vapor
+/// 1
+import FluentPostgreSQL
+
+public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
+    /// 2
+    try services.register(FluentPostgreSQLProvider())
+
+    let router = EngineRouter.default()
+    try routes(router)
+    services.register(router, as: Router.self)
+    var middlewares = MiddlewareConfig() // Create _empty_ middleware config
+    middlewares.use(ErrorMiddleware.self)
+    services.register(middlewares)
+
+    /// 3
+    let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
+    let username = Environment.get("DATABASE_USER") ?? "vapor"
+    let databaseName: String;
+    let databasePort: Int;
+    let password = Environment.get("DTABASE_PASSWORD") ?? "password"
+    if env == .testing {
+        databaseName = "vapor-test"
+//        databasePort = 5433;
+        if let testPort = Environment.get("DATABASE_PORT") {
+            databasePort = Int(testPort) ?? 5433;
+        } else {
+            databasePort = 5433;
+        }
+    } else {
+        databaseName = Environment.get("DATABASE_DB") ?? "vapor"
+        databasePort = 5432;
+    }
+    
+    
+    
+    let postgreSQLDatabaseConfig = PostgreSQLDatabaseConfig(hostname: hostname, port: databasePort, username: username, database: databaseName, password: password);
+    let postgresDatabase = PostgreSQLDatabase(config: postgreSQLDatabaseConfig);
+    
+    var databases = DatabasesConfig()
+//    databases.add(database: sqlite, as: .sqlite)
+    ///4
+    databases.add(database: postgresDatabase, as: .psql)
+    services.register(databases)
+
+    /// 5
+    var migrations = MigrationConfig()
+    migrations.add(model: User.self, database: .psql)
+    migrations.add(model: Acronym.self, database: .psql)
+    migrations.add(model: Category.self, database: .psql)
+    migrations.add(model: AcronymCategoryPivot.self, database: .psql)
+    services.register(migrations)
+    
+    var commandConfig = CommandConfig.default();
+    commandConfig.useFluentCommands();
+    services.register(commandConfig);
+
+}
---
+++ Tests/AppTests/UserTests.swift
@@ -0,0 +1,161 @@
+//
+//  UserTests.swift
+//  App
+//
+//  Created by apple on 2019/2/5.
+//
+
+import Foundation
+
+@testable import App
+import Vapor
+import XCTest
+import FluentPostgreSQL
+
+final class UserTests: XCTestCase {
+    /*
+    func testUserscanBeRetrievedFromAPI() throws{
+        /// 复原数据库(清空数据库)
+        // 1
+        let revertEnvironmentArgs = ["vapor", "revert", "--all", "-y"]
+        // 2
+        var revertConfig = Config.default()
+        var revertServices = Services.default()
+        var revertEnv = Environment.testing
+        // 3
+        revertEnv.arguments = revertEnvironmentArgs
+        // 4
+        try App.configure(&revertConfig, &revertEnv, &revertServices)
+        let revertApp = try Application(
+            config: revertConfig,
+            environment: revertEnv,
+            services: revertServices)
+        try App.boot(revertApp)
+        // 5
+        try revertApp.asyncRun().wait()
+        // 6
+        let migrateEnvironmentArgs = ["vapor", "migrate", "-y"]
+        var migrateConfig = Config.default()
+        var migrateServices = Services.default()
+        var migrateEnv = Environment.testing
+        migrateEnv.arguments = migrateEnvironmentArgs
+        try App.configure(&migrateConfig, &migrateEnv, &migrateServices)
+        let migrateApp = try Application(
+            config: migrateConfig,
+            environment: migrateEnv,
+            services: migrateServices)
+        try App.boot(migrateApp)
+        try migrateApp.asyncRun().wait()
+        ////////////////////////////////////////////////////////////////
+        
+        /// 保存一个用户到数据库
+        let expectedName = "Alice"
+        let expectedUsername = "alice"
+        
+        var config = Config.default();
+        var services = Services.default();
+        var env = Environment.testing;
+        try App.configure(&config, &env, &services)
+        let app = try Application(config: config, environment: env, services: services);
+        try App.boot(app);
+        let conn = try app.newConnection(to: .psql).wait();
+        
+        let user = User(name: expectedName, username: expectedUsername)
+        let saveUser = try user.save(on: conn).wait();
+        
+        /// 保存第二个用户到数据库
+        _ = try User(name: "Luke", username: "lukes").save(on: conn).wait;
+        let responder = try app.make(Responder.self);
+        /// 获取所有用户
+        let request = HTTPRequest(method: .GET, url: URL(string: "/api/users")!)
+        let wrappedRequest = Request(http: request, using: app)
+        let response = try responder.respond(to: wrappedRequest).wait()
+        let data = response.http.body.data;
+        let users = try JSONDecoder().decode([User].self, from: data!)
+        
+        XCTAssertEqual(users.count, 2);
+        XCTAssertEqual(users[0].name, expectedName)
+        XCTAssertEqual(users[0].username, expectedUsername)
+        XCTAssertEqual(users[0].id, saveUser.id)
+        
+        /// 关闭数据库连接
+        conn.close();
+    }
+ */
+    let usersName = "Alice";
+    let userUsername = "alicea";
+    let usersURI = "/api/users/";
+    var app: Application!
+    var conn: PostgreSQLConnection!
+    
+    override func setUp() {
+        try! Application.reset();
+        app = try! Application.testable();
+        conn = try! app.newConnection(to: .psql).wait();
+    }
+    
+    override func tearDown() {
+        conn.close();
+    }
+    
+    func testUsersCanBeRetrievedFromAPI() throws {
+        let user = try User.create(name: usersName, username: userUsername, on: conn)
+        _ = try User.create(on: conn)
+        
+        let users = try app.getResponse(to: usersURI, decodeTo: [User].self);
+        
+        XCTAssertEqual(users.count, 2)
+        XCTAssertEqual(users[0].name, usersName);
+        XCTAssertEqual(users[0].username, userUsername);
+        XCTAssertEqual(users[0].id, user.id);
+    }
+    
+    func testUserCanBeSavedWithAPI() throws {
+        let user = User(name: usersName, username: userUsername)
+        let receivedUser = try app.getResponse(to: usersURI, method: .POST, headers: ["Content-Type":"application/json"], data: user, decodeTo: User.self)
+        
+        XCTAssertEqual(receivedUser.name, usersName);
+        XCTAssertEqual(receivedUser.username, userUsername);
+        XCTAssertNotNil(receivedUser.id);
+        
+        let users = try app.getResponse(to: usersURI, decodeTo: [User].self);
+        
+        XCTAssertEqual(users.count, 1)
+        XCTAssertEqual(users[0].name, usersName)
+        XCTAssertEqual(users[0].username, userUsername)
+        XCTAssertEqual(users[0].id, receivedUser.id)
+    }
+    
+    func testGettingASingleUserFromTheAPI() throws {
+        let user = try User.create(name: usersName, username: userUsername, on: conn);
+        let receivedUser = try app.getResponse(to: "\(usersURI)\(user.id!)",decodeTo: User.self);
+        XCTAssertEqual(receivedUser.name, usersName);
+        XCTAssertEqual(receivedUser.username, userUsername);
+        XCTAssertEqual(receivedUser.id, user.id);
+    }
+    
+    func testGettingAUsersAcronymsFromTheAPI() throws {
+        let user = try User.create(on: conn);
+        let acronymShort = "OMG"
+        let acronymLong = "Oh My God";
+        let acronym1 = try Acronym.create(short: acronymShort, long: acronymLong, user: user, on: conn);
+        _ = try Acronym.create(short: "LOL", long: "Laugh Out Loud", user: user, on: conn)
+        
+        let acronyms = try app.getResponse(to: "\(usersURI)\(user.id!)/acronyms", decodeTo: [Acronym].self);
+        
+        XCTAssertEqual(acronyms.count, 2)
+        XCTAssertEqual(acronyms[0].id, acronym1.id)
+        XCTAssertEqual(acronyms[0].short, acronymShort)
+        XCTAssertEqual(acronyms[0].long, acronymLong)
+    }
+    
+    static let allTests = [
+        ("testUsersCanBeRetrievedFromAPI",
+         testUsersCanBeRetrievedFromAPI),
+        ("testUserCanBeSavedWithAPI", testUserCanBeSavedWithAPI),
+        ("testGettingASingleUserFromTheAPI",
+         testGettingASingleUserFromTheAPI),
+        ("testGettingAUsersAcronymsFromTheAPI",
+         testGettingAUsersAcronymsFromTheAPI)
+    ]
+}
